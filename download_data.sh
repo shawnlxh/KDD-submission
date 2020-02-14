da2204d6863315cf6aee9a7513011bad614baefe
shawnlxh <shawnlxh@outlook.com>
2020-02-14 01:46:10 -0600

    Add files via upload

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://snap.stanford.edu/jodie/reddit.csv -P data/
+wget http://snap.stanford.edu/jodie/wikipedia.csv -P data/
+wget http://snap.stanford.edu/jodie/lastfm.csv -P data/
